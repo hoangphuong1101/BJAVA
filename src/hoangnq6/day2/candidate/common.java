5caf13b438813b33c7eaa5ef2bc4568b768e3feb
HCD-Fresher271 <HCD-Fresher271@Temp-Fresher800.fsoft.fpt.vn>
2017-08-08 16:45:07 +0700

    dau

---
+++ src/hoangnq6/day2/candidate/common.java
@@ -0,0 +1,18 @@
+package hoangnq6.day2.candidate;
+/**			
+ * FresherCandidate			
+ *			
+ * Version 1.0			
+ *			
+ * Date: 08-08-2017			
+ *			
+ * Copyright 			
+ *			
+ * Modification Logs:			
+ * DATE                 AUTHOR          DESCRIPTION			
+ * -----------------------------------------------------------------------			
+ * 08-08-2017         HoangNQ6            Create			
+ */
+public class common {
+	
+}
